cc20b811b365a298827ab3aeb7fdd51152b587cf
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-08-09 20:43:39 -0400

    adding react + pest

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Dashboard1') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
