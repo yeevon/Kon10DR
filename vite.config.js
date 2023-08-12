10093a7a8212df07a68577f063c21516be016ce4
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-08-11 23:29:36 -0400

    small changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: '127.0.0.1',
+        },
+    },
     plugins: [
         laravel({
             input: [
