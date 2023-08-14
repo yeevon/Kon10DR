6128a012f7763ab3dca87648d5ac471932f7cfa1
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-08-14 00:47:35 -0400

    docker

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
@@ -10,4 +10,14 @@ export default defineConfig({
         }),
         react(),
     ],
+    server: {
+        hmr: {host: 'localhost'},
+        port: 3000,
+        watch: {
+            usePolling: true,
+        },
+        host: true, // Here
+    },
+
+
 });
