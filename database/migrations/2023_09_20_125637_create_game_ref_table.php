17fc144c8ff983f9d9d01bb43f8db0ece042102c
Jose de Lima <30577072+yeevon@users.noreply.github.com>
2023-09-20 09:51:23 -0500

    Update database/migrations/2023_09_20_125637_create_game_ref_table.php
    Co-authored-by: Elminson De Oleo Baez <edeoleo@gmail.com>

--- database/migrations/2023_09_20_125637_create_game_ref_table.php
+++ database/migrations/2023_09_20_125637_create_game_ref_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('game_ref', function (Blueprint $table) {
+        Schema::create('game_user', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('game_id');
