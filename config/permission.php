df12dbb2289fb30dbb10daafdd0054cbc857cfee
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-10-29 23:07:01 -0400

    Change the table name for 'roles' to 'spatie_roles' in config/permission.php

--- config/permission.php
+++ config/permission.php
@@ -36,7 +36,7 @@ return [
          * default value but you may easily change it to any table you like.
          */
 
-        'roles' => 'roles',
+        'roles' => 'spatie_roles',
 
         /*
          * When using the "HasPermissions" trait from this package, we need to know which
