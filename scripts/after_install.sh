4aee13e4e646a98f910704240aa90f994b99ba72
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-10-29 21:05:07 -0400

    Update composer installation and file permissions in after_install.sh and before_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo chmod 777 /var/app/current/vendor -R
+sudo chmod 777 /var/app/current/storage -R
+sudo mkdir /var/app/current/bootstrap/cache
+sudo chomd 777 /var/app/current/bootstrap -R
 sudo cp /opt/elasticbeanstalk/deployment/env /var/www/html/.env && sudo chown ec2-user:webapp /var/www/html/.env && sudo chmod 644 /var/www/html/.env
 sudo chown webapp:webapp /var/app/current -R
-php composer install
+php /usr/local/bin/composer install
 sudo npm install vite
 npm install
 sudo npm run build
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo "Before Install"
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
